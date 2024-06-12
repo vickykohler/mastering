6e20b973c88bb0439c2449f8352e7c7080b36a94
lerryleer <lerryleer@gmail.com>
2024-06-12 06:30:19 +0000

    main

---
+++ ai mastering.zip_unzipped/ai mastering/mastering/configa03c.js
@@ -0,0 +1,35 @@
+BAKUAGE_CLIENT_CONFIG = {
+  analytics: {
+    id: 'UA-78475193-3',
+    exception: true,
+  },
+  api: {
+    baseUrl: 'https://api.bakuage.com',
+  },
+  auth0: {
+    logo: 'https://aimastering.com/assets/images/logo/png/icon_light_blue.png',
+  },
+  link: {
+    appHome: {
+      ja: 'https://app.bakuage.com/',
+      en: 'https://app.bakuage.com/',
+      // en: 'https://aimastering.com/app/',
+    },
+    home: {
+      ja: 'https://bakuage.com/',
+      en: 'https://aimastering.com/',
+    },
+    commercialNotation: {
+      ja: 'https://bakuage.com/commercial_notation/',
+      en: 'https://aimastering.com/commercial_notation/',
+    },
+    termsOfService: {
+      ja: 'https://bakuage.com/terms_of_service/',
+      en: 'https://aimastering.com/terms_of_service/',
+    },
+    privacyPolicy: {
+      ja: 'https://bakuage.com/privacy_policy/',
+      en: 'https://aimastering.com/privacy_policy/',
+    },
+  }
+}
